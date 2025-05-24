831d1a709ba3cb2b74561d01a8fc68f1edc06371
James Green <jgreenop22@gmail.com>
2025-05-23 20:29:36 -0700

    init

---
+++ src/main/java/g_mungus/DeepSpaceMod.java
@@ -0,0 +1,14 @@
+package g_mungus;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DeepSpaceMod.MOD_ID)
+public final class DeepSpaceMod {
+    public static final String MOD_ID = "deep_space";
+
+    public DeepSpaceMod() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like registries and resources) may still be uninitialized.
+        // Proceed with mild caution.
+    }
+}
